f3d6e0fb98c08b5fc7ba0bca856bc83f9e9f59f4
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-04-18 18:45:38 -0500

    complete css

--- javascript/music.js
+++ javascript/music.js
@@ -5,45 +5,49 @@ window.onload = function () {
     const search = document.getElementById("btn-search")
 
     search.addEventListener('click', function () {
-        const searchInput = document.getElementById("input-search").value;
-        let dataTable = document.getElementById('song-table');
-
-        fetch(`http://localhost:9999/songs?songName=${searchInput}`, {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-                'token': sessionStorage.getItem("token"),
-            },
-        })
-            .then((response) => {
-                if (response.status == 500) {
-                    throw Error("Sorry, something went wrong!!!")
-                } else {
-                    return response.json();
-                }
+        let searchInput = document.getElementById("input-search").value;
+        if(searchInput=='' ||searchInput==' '){
+            searchInput=undefined;
+        }
+        if ( searchInput != undefined) {
+            let dataTable = document.getElementById('song-table');
+            fetch(`http://localhost:9999/songs?songName=${searchInput}`, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    'token': sessionStorage.getItem("token"),
+                },
             })
-            .then((json) => {
-                let tableHeaderRowCount = 1;
-                let rowCount = dataTable.rows.length;
-                for (let i = tableHeaderRowCount; i < rowCount; i++) {
-                    dataTable.deleteRow(tableHeaderRowCount);
-                }
-                console.log(json);
-                const tblData = json;
-                console.log(tblData[0])
-                console.log(tblData[0].id)
+                .then((response) => {
+                    if (response.status == 500) {
+                        throw Error("Sorry, something went wrong!!!")
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then((json) => {
+                    let tableHeaderRowCount = 1;
+                    let rowCount = dataTable.rows.length;
+                    for (let i = tableHeaderRowCount; i < rowCount; i++) {
+                        dataTable.deleteRow(tableHeaderRowCount);
+                    }
+                    console.log(json);
+                    const tblData = json;
+                    console.log(tblData[0])
+                    console.log(tblData[0].id)
 
-                for (let i = 0; i < tblData.length; i++) {
-                    dataTable.innerHTML += "<tr><td>" + tblData[i].id + "</td>" +
-                        "<td>" + tblData[i].title + "</td>" +
-                        "<td>" + tblData[i].releaseDate + "</td>" +
-                        "<td><button id='add' onclick='addSong(this)'>Add</button></td>" +
-                        "</tr>"
-                }
-            }).catch(error => {
-            console.log(error)
-            document.getElementById("welcome").innerText = "Sorry, incorrect Username or password!!!!";
-        });
+                    for (let i = 0; i < tblData.length; i++) {
+                        dataTable.innerHTML += "<tr><td>" + tblData[i].id + "</td>" +
+                            "<td>" + tblData[i].title + "</td>" +
+                            "<td>" + tblData[i].releaseDate + "</td>" +
+                            "<td><button id='add' onclick='addSong(this)'>Add</button></td>" +
+                            "</tr>"
+                    }
+                }).catch(error => {
+                console.log(error)
+                document.getElementById("welcome").innerText = "Sorry, incorrect Username or password!!!!";
+            });
+        }
     });
 
     logout.addEventListener('click', function () {
@@ -109,7 +113,7 @@ window.onload = function () {
                 const tblData = json;
                 console.log(tblData)
 
-                if(tblData.length>0){
+                if (tblData.length > 0) {
                     for (let i = 0; i < tblData.length; i++) {
                         dataTable.innerHTML += "<tr><td>" + tblData[i].songId + "</td>" +
                             "<td>" + tblData[i].title + "</td>" +
@@ -118,8 +122,8 @@ window.onload = function () {
                             "<button id='play' onclick='displayIframe(this)' >Play</button></td>" +
                             "</tr>"
                     }
-                }else {
-                    dataTable.innerHTML='<p>Playlist empty</p>';
+                } else {
+                    dataTable.innerHTML = '<p>Playlist empty</p>';
                 }
 
             }).catch(error => {
