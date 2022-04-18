d6426bdfc25f77c6e02bd2a0b22e120225084768
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-04-18 16:04:52 -0500

    complete adding in playlist

---
+++ javascript/add.js
@@ -0,0 +1,50 @@
+function upTo(node, tagName) {
+    if (!node || !tagName) return;
+    tagName = ('' + tagName).toLowerCase();
+    while ((node = node.parentNode) && node.tagName) {
+        if (node.tagName.toLowerCase() == tagName) {
+            return node;
+        }
+    }
+    return null;
+}
+
+function addSong(node) {
+    const row = upTo(node, 'tr');
+   const songId=row.parentNode.querySelector('td').innerText;
+   console.log(songId)
+    fetch(`http://localhost:9999/playList?songId=${songId}`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'token': sessionStorage.getItem("token"),
+        },
+    })
+        .then((response) => {
+            if (response.status == 500) {
+                throw Error("Sorry, something went wrong!!!")
+            } else {
+                return response.json();
+            }
+        })
+        .then((json) => {
+            console.log(json);
+            const tblData = json;
+            console.log(tblData[0])
+            console.log(tblData[0].id)
+
+            for (let i = 0; i < tblData.length; i++) {
+                dataTable.innerHTML += "<tr><td>" + tblData[i].songId + "</td>" +
+                    "<td>" + tblData[i].title + "</td>" +
+                    "<td>" + tblData[i].releaseDate + "</td>" +
+                    "<td><button id='remove'>Delete</button><button id='play'>Play</button></td>" +
+                    "</tr>"
+            }
+        }).catch(error => {
+        console.log(error)
+        document.getElementById("welcome").innerText = "Sorry, incorrect Username or password!!!!";
+    });
+    location.reload();
+
+}
+
--- javascript/music.js
+++ javascript/music.js
@@ -1,6 +1,10 @@
 window.onload = function () {
-    getPlaylist();
+    getSonglist();
+    getPlayList();
     const logout = document.getElementById('logout');
+    const remove = document.getElementById('delete');
+    const add = document.getElementById('add');
+    const play = document.getElementById('play');
 
     logout.addEventListener('click', function () {
         sessionStorage.clear();
@@ -8,7 +12,7 @@ window.onload = function () {
     })
 
 
-    function getPlaylist() {
+    function getSonglist() {
         let dataTable = document.getElementById('song-table');
         fetch('http://localhost:9999/songs', {
             method: 'POST',
@@ -26,15 +30,51 @@ window.onload = function () {
             })
             .then((json) => {
                 console.log(json);
-                const tblData=json;
+                const tblData = json;
                 console.log(tblData[0])
                 console.log(tblData[0].id)
 
-                for(let i=0;i<tblData.length;i++){
-                    dataTable.innerHTML += "<tr><td>"+tblData[i].id+"</td>" +
-                        "<td>"+tblData[i].title+"</td>" +
-                        "<td>"+tblData[i].releaseDate+"</td>" +
-                        "<td><button id='add'>Add</button></td>" +
+                for (let i = 0; i < tblData.length; i++) {
+                    dataTable.innerHTML += "<tr><td>" + tblData[i].id + "</td>" +
+                        "<td>" + tblData[i].title + "</td>" +
+                        "<td>" + tblData[i].releaseDate + "</td>" +
+                        '<td><button id="add" onclick="addSong(this)">Add</button></td>' +
+                        "</tr>"
+                }
+            }).catch(error => {
+            console.log(error)
+            document.getElementById("welcome").innerText = "Sorry, incorrect Username or password!!!!";
+        });
+    }
+
+    function getPlayList() {
+        let dataTable = document.getElementById('playlist-table');
+        console.log("inside playlist");
+        fetch('http://localhost:9999/playList', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'token': sessionStorage.getItem("token"),
+            },
+        })
+            .then((response) => {
+                if (response.status == 500) {
+                    throw Error("Sorry, something went wrong!!!")
+                } else {
+                    return response.json();
+                }
+            })
+            .then((json) => {
+                console.log(json);
+                const tblData = json;
+                console.log(tblData[0])
+                console.log(tblData[0].id)
+
+                for (let i = 0; i < tblData.length; i++) {
+                    dataTable.innerHTML += "<tr><td>" + tblData[i].songId + "</td>" +
+                        "<td>" + tblData[i].title + "</td>" +
+                        "<td>" + tblData[i].releaseDate + "</td>" +
+                        "<td><button id='remove'>Delete</button><button id='play'>Play</button></td>" +
                         "</tr>"
                 }
             }).catch(error => {
@@ -43,3 +83,4 @@ window.onload = function () {
         });
     }
 }
+
