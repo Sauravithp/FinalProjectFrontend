96065b7f176dc88127226d1cbc9c7502fedb70f6
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-04-18 14:27:19 -0500

    complete adding data on table

--- javascript/music.js
+++ javascript/music.js
@@ -1,8 +1,45 @@
 window.onload = function () {
+    getPlaylist();
     const logout = document.getElementById('logout');
 
-    logout.addEventListener('click',function (){
+    logout.addEventListener('click', function () {
         sessionStorage.clear();
         location.replace('./app.html')
     })
+
+
+    function getPlaylist() {
+        let dataTable = document.getElementById('song-table');
+        fetch('http://localhost:9999/songs', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'token': sessionStorage.getItem("token"),
+            },
+        })
+            .then((response) => {
+                if (response.status == 500) {
+                    throw Error("Sorry, something went wrong!!!")
+                } else {
+                    return response.json();
+                }
+            })
+            .then((json) => {
+                console.log(json);
+                const tblData=json;
+                console.log(tblData[0])
+                console.log(tblData[0].id)
+
+                for(let i=0;i<tblData.length;i++){
+                    dataTable.innerHTML += "<tr><td>"+tblData[i].id+"</td>" +
+                        "<td>"+tblData[i].title+"</td>" +
+                        "<td>"+tblData[i].releaseDate+"</td>" +
+                        "<td><button id='add'>Add</button></td>" +
+                        "</tr>"
+                }
+            }).catch(error => {
+            console.log(error)
+            document.getElementById("welcome").innerText = "Sorry, incorrect Username or password!!!!";
+        });
+    }
 }
